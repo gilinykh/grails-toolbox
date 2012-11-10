d35ab80db938663eb1c7a80a286fe97ee63f94af
Igor Sinev <isinev@gmail.com>
2012-11-10 18:38:21 +0400

    removed dependency from kickstart, copied files instead

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -4,7 +4,8 @@ modules = {
         resource url: 'css/bootswatcher.css', disposition: 'head'
     }
     
-    if (grails.util.Environment.current == grails.util.Environment.PRODUCTION) {
+    if (2 * 2 == 4) {
+    //if (grails.util.Environment.current == grails.util.Environment.PRODUCTION) {
        'grails-toolbox-css' {
             dependsOn 'grails-toolbox-bootswatcher-css'
             resource url: 'css/grails-toolbox.css', disposition: 'head'
